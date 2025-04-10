60c1ad2f0d871c326c253a668949ddb916873611
keerthiprasath3801 <keerthiprasath2001@gmail.com>
2025-04-10 12:30:00 +0530

    1.post routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js"
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.route.js"
 import dotenv from "dotenv"
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -19,6 +20,7 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
 app.listen(PORT,() =>{
     console.log(`Server is running on Port ${PORT}`)
     connectMongoDB();
