7af66fc5ed2f4acbb1840caf8eaadb65ee04cee4
keerthiprasath3801 <keerthiprasath2001@gmail.com>
2025-04-11 13:10:14 +0530

    buildupdate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,6 @@ function App() {
 
 	return (
 		<div className='flex max-w-6xl mx-auto'>
-			{/* Common component, bc it's not wrapped with Routes */}
 			{authUser && <Sidebar />}
 			<Routes>
 				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
