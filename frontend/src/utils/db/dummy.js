aa56c08f262d3094789d2c2e2fedb1c8ac0a71b9
keerthiprasath3801 <keerthiprasath2001@gmail.com>
2025-04-11 06:46:31 +0530

    1-ui design completed

---
+++ frontend/src/utils/db/dummy.js
@@ -0,0 +1,128 @@
+export const POSTS = [
+    {
+      _id: "1",
+      text: "Calculating the most efficient way to dominate this class... just like I planned. 🧠 #ClassroomOfTheElite",
+      img: "/posts/post1.jpg",
+      user: {
+        username: "kiyotaka",
+        profileImg: "/avatars/kiyotaka.jpg",
+        fullName: "Kiyotaka Ayanokōji",
+      },
+      comments: [
+        {
+          _id: "1",
+          text: "You always think 10 steps ahead, don't you?",
+          user: {
+            username: "suzune",
+            profileImg: "/avatars/suzune.jpg",
+            fullName: "Suzune Horikita",
+          },
+        },
+      ],
+      likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+    },
+    {
+      _id: "2",
+      text: "NOT HAVING MAGIC WON'T STOP ME! I'LL BECOME THE WIZARD KING! 💪🏽⚔️ #BlackClover",
+      user: {
+        username: "asta",
+        profileImg: "/avatars/asta.jpg",
+        fullName: "Asta",
+      },
+      comments: [
+        {
+          _id: "1",
+          text: "Hmph. You're still as loud as ever, Asta.",
+          user: {
+            username: "yuno",
+            profileImg: "/avatars/yuno.jpg",
+            fullName: "Yuno Grinberryall",
+          },
+        },
+      ],
+      likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+    },
+    {
+      _id: "3",
+      text: "Another day, another pile of paperwork. Where's my damn coffee? ☕ #CaptainLife",
+      img: "/posts/post2.jpeg",
+      user: {
+        username: "yami",
+        profileImg: "/avatars/yami.jpg",
+        fullName: "Yami Sukehiro",
+      },
+      comments: [],
+      likes: ["6658s891", "6658s892", "6658s893", "6658s894", "6658s895", "6658s896"],
+    },
+    {
+      _id: "4",
+      text: "I'll create a new world... starting with this code. The God of Programming. 💻 #JustKiraThings",
+      img: "/posts/post3.jpg",
+      user: {
+        username: "light",
+        profileImg: "/avatars/light.jpg",
+        fullName: "Light Yagami",
+      },
+      comments: [
+        {
+          _id: "1",
+          text: "Light-sama, you're so cool! 😍 Can I help?",
+          user: {
+            username: "misa",
+            profileImg: "/avatars/misa.jpg",
+            fullName: "Misa Amane",
+          },
+        },
+      ],
+      likes: [
+        "6658s891",
+        "6658s892",
+        "6658s893",
+        "6658s894",
+        "6658s895",
+        "6658s896",
+        "6658s897",
+        "6658s898",
+        "6658s899",
+      ],
+    },
+  ];
+  
+  export const USERS_FOR_RIGHT_PANEL = [
+    {
+      _id: "1",
+      fullName: "Kiyotaka Ayanokōji",
+      username: "kiyotaka",
+      profileImg: "/avatars/kiyotaka.jpg",
+    },
+    {
+      _id: "2",
+      fullName: "Suzune Horikita",
+      username: "suzune",
+      profileImg: "/avatars/suzune.jpg",
+    },
+    {
+      _id: "3",
+      fullName: "Asta",
+      username: "asta",
+      profileImg: "/avatars/asta.jpg",
+    },
+    {
+      _id: "4",
+      fullName: "Yuno Grinberryall",
+      username: "yuno",
+      profileImg: "/avatars/yuno.jpg",
+    },
+    {
+      _id: "5",
+      fullName: "Yami Sukehiro",
+      username: "yami",
+      profileImg: "/avatars/yami.jpg",
+    },
+    {
+      _id: "6",
+      fullName: "Light Yagami",
+      username: "light",
+      profileImg: "/avatars/light.jpg",
+    },
+  ];
